284bcb1c6903ca4aa997fbc5004c5bdab23a6a17
kushan <kushan_chinthaka@persistent.com>
2017-06-22 04:31:32 +0530

    e

--- src/com/examples/controller/HomeController.java
+++ src/com/examples/controller/HomeController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping(value = "/Home")
 public class HomeController {
-	@RequestMapping({"/", "/home"}) // /Pay/ or /Pay/home
+	@RequestMapping({"/", "/home"}) // / or /home
 	public String  showHomePage() {
 		return "home"; // logical view name
 	}
